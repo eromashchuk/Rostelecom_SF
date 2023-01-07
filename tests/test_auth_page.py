35469f5c8201417fb80c4168aa8ab50462b53964
E.Romashchuk <chudin@gmail.com>
2023-01-07 22:24:18 +0300

    Added changes to files

--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -25,11 +25,10 @@ from pages.auth_page import AuthPage
 class TestAuthPositive:
     ''' Positive tests for Auth page test cases '''
 
-    # Проверка смены вкладок авторизации и смену подсказок в инпуте(по телефону по-умолчанию)
-    @pytest.mark.skip
+    # TK-004 Проверка смены вкладок авторизации и смену подсказок в инпуте
     def test_auth_tabs_default(self, web_browser):
         page = AuthPage(web_browser)
-        assert page.placeholder_login.get_text() == 'Мобильный телефон'
+        assert page.placeholder_login.get_text() == 'Мобильный телефон' #(TK-005 вкладка по телефону по-умолчанию)
         assert page.placeholder_pswrd.get_text() == 'Пароль'
         page.by_email.click()
         assert page.placeholder_login.get_text() == 'Электронная почта'
@@ -38,8 +37,7 @@ class TestAuthPositive:
         page.by_LS.click()
         assert page.placeholder_login.get_text() == 'Лицевой счёт'
 
-    # Проверка автосмены вкладок при вводе логина
-    @pytest.mark.skip
+    # TK-006 Проверка автосмены вкладок при вводе логина
     def test_autochange_of_auth_tabs(self, web_browser):
         page = AuthPage(web_browser)
         # Input phone_number to the login field
@@ -60,7 +58,7 @@ class TestAuthPositive:
         assert page.placeholder_login.get_text() == 'Логин'
         page.login.clear()
 
-    # Успешная авторизация по email
+    # TK-010 Успешная авторизация по email (не стала делать параметризацию, но подразумеваю)
     def test_successful_login_by_email(self, web_browser, email='chudin.v@gmail.com', password='A123456789a'):
         page = AuthPage(web_browser)
         page.login.send_keys(email)
@@ -70,9 +68,10 @@ class TestAuthPositive:
         next_page = page.get_current_url()
         assert 'https://b2c.passport.rt.ru/account_b2c/page' in next_page
 
-    # Упешная авторизация по номеру телефона:
-    @pytest.mark.skip
-    def test_successful_login_by_phone(self, web_browser, phone='79787569117', password='A123456789a'):
+    # TK-007, TK-008, TK-009 Упешная авторизация по номеру телефона, нельзя ввести более 11 символов:
+    @pytest.mark.parametrize("phone", ['79787569117', '89787569117', '797875691171']
+        , ids=['valid phone starts from 7', 'valid phone starts from 8', 'valid phone +1']) #Тут можно добавить номер для +375, но у меня нет
+    def test_successful_login_by_phone(self, web_browser, phone, password='A123456789a'):
         page = AuthPage(web_browser)
         page.login.send_keys(phone)
         page.password.send_keys(password)
@@ -81,7 +80,8 @@ class TestAuthPositive:
         next_page = page.get_current_url()
         assert 'https://b2c.passport.rt.ru/account_b2c/page' in next_page
 
-    @pytest.mark.skip(reason='too much time need')
+    #Проверка валидации ввода в поле Логина, принимает любые символы
+    @pytest.mark.skip(reason='too much time need to check, but it work')
     @pytest.mark.xfail(reason='Known bug: phone-login field not allow some special characters')
     @pytest.mark.parametrize("login", [generate_string(255), alphabet_chars(), alphabet_chars().upper()
         , russian_chars(), russian_chars().upper()
@@ -94,25 +94,24 @@ class TestAuthPositive:
         text = page.login_value.get_attribute(attr_name='innerHTML')
         assert str(login) in text
 
-    # Проверка ссылки пользовательского соглашения
-    @pytest.mark.skip
+    # ТК-023 Проверка ссылки пользовательского соглашения
+    @pytest.mark.xfail(reason='Known bug for naming of agreement, need capital letter')
     def test_successful_redirect_to_contract(self, web_browser):
         page = AuthPage(web_browser)
         text = page.auth_policy.get_attribute(attr_name='innerHTML')
-        assert 'Пользовательского соглашения' in text
+        assert 'Пользовательского Соглашения' in text
         assert page.auth_policy.is_clickable()
         page.auth_policy.click()
         next_page = page.auth_policy.get_attribute(attr_name='href')
         assert 'https://b2c.passport.rt.ru/sso-static/agreement/agreement.html' in next_page
 
-    @pytest.mark.skip
     # Проверка перехода в форму восстановления пароля("Забыл пароль")
     def test_fogot_password_link(self, web_browser):
         page = AuthPage(web_browser)
         page.forgot_pswrd_link.click()
         assert page.forgot_password_form.is_presented()
         assert page.forgot_password_form.get_attribute(attr_name='innerHTML') == 'Восстановление пароля'
-    @pytest.mark.skip
+
     # Проверка перехода в форму Регистрации
     def test_register_link(self, web_browser):
         page = AuthPage(web_browser)
@@ -120,8 +119,7 @@ class TestAuthPositive:
         assert page.register_form.is_presented()
         assert page.register_form.get_attribute(attr_name='innerHTML') == 'Регистрация'
 
-    @pytest.mark.skip
-    # Проверка авторизации церез соц.сети
+    # TK-017, TK-018, TK-019, TK-020, TK-021 Проверка авторизации церез oauth соц.сети
     def test_auth_by_social_services(self, web_browser):
         page = AuthPage(web_browser)
         # Авторизация через Вконтакте
@@ -153,8 +151,9 @@ class TestAuthPositive:
 class TestAuthNegative:
     ''' Negative tests for Auth page test cases '''
 
+    #TK-011, TK-013, TK-015 Нельзя авторизоваться с неверным номером телефона, имейлом, логином, лицевым счетом
     @pytest.mark.parametrize("login"
-        , ['shumak@gmail.com', 'test@test', '9787569118', 'w_login']
+        , ['shumak@gmail.com', 'test@test', '7978756911', 'w_login']
         , ids=['existing email', 'wrong email', 'wrong phone number', 'wrong login'])
     #@pytest.mark.parametrize("password", ['A123456789a', '*'], ids=['existing password for other profile', 'wrong password'])
     def test_unsuccessful_login_by_wrong_login_password(self, web_browser, login, password='*'):
@@ -169,7 +168,7 @@ class TestAuthNegative:
         assert page.forgot_orange_link.is_presented()
         assert page.forgot_pswrd_link.is_presented()
 
-    # Проверка сообщения об ошибки для пустого логина (для пароля не реализовано)
+    # TK-022 Проверка сообщения об ошибке для пустого логина (для пароля не реализовано BUG-07)
     def test_message_for_empty_login(self, web_browser):
         page = AuthPage(web_browser)
         page.submit_btn.click()
